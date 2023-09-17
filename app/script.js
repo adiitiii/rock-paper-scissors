992f16ef291369fd6500b5c55045e26277450866
Aditi Maheshwari <adimaheshwari2207@gmail.com>
2023-09-17 01:33:35 -0400

    Commit some changes again

--- app/script.js
+++ app/script.js
@@ -25,17 +25,46 @@ function getComputerChoice() {
 const computerSelection = getComputerChoice();
 // //Function playRound
 
-const playRound = function (playerChoice, computerChoice) {
+const rockOption = document.getElementById('✊');
+const paperOption = document.getElementById('🤚');
+const scissorOption = document.getElementById('✌');
+const playerChoiceEl = document.getElementById('rps-output-player');
+
+rockOption.addEventListener('click', function () {
+    console.log('Rock option clicked');
+    playRound('✊', computerSelection);
+    playerSelection = '✊';
+    playerChoiceEl.textContent = playerSelection;
+    return playerSelection;
+});
+
+paperOption.addEventListener('click', function () {
+    console.log('Paper option clicked');
+    playRound('🤚', computerSelection);
+    playerSelection = '🤚';
+    playerChoiceEl.textContent = playerSelection;
+    return playerSelection;
+});
+
+scissorOption.addEventListener('click', function () {
+    console.log('Scissors option clicked');
+    playRound('✌', computerSelection);
+    playerSelection = '✌';
+    playerChoiceEl.textContent = playerSelection;
+    return playerSelection;
+});
+
+const playRound = function (playerSelection, computerSelection) {
     if (playing) {
         // Check for Rock✊ and Paper✋
-        if (playerChoice === '✊' && computerChoice === '✋') {
+        if (playerSelection === '✊' && computerSelection === '✋') {
             return [
                 {
                     winningMessage: 'You Lose! Paper beats Rock',
                     winner: 'computer',
                 },
             ];
-        } else if (playerChoice === '✋' && computerChoice === '✊') {
+        } else if (playerSelection === '✋' && computerSelection === '✊') {
             return [
                 {
                     winningMessage: 'You Won! Paper beats Rock',
@@ -45,14 +74,14 @@ const playRound = function (playerChoice, computerChoice) {
         }
 
         // Check for Rock✊ and Scissors✌
-        else if (playerChoice === '✊' && computerChoice === '✌') {
+        else if (playerSelection === '✊' && computerSelection === '✌') {
             return [
                 {
                     winningMessage: 'You Won! Rock beats Scissors',
                     winner: 'player',
                 },
             ];
-        } else if (playerChoice === '✌' && computerChoice === '✊') {
+        } else if (playerSelection === '✌' && computerSelection === '✊') {
             return [
                 {
                     winningMessage: 'You Lose! Rock beats Scissors',
@@ -62,14 +91,14 @@ const playRound = function (playerChoice, computerChoice) {
         }
 
         // Check for Paper✋ and Scissors✌
-        else if (playerChoice === '✋' && computerChoice === '✌') {
+        else if (playerSelection === '✋' && computerSelection === '✌') {
             return [
                 {
                     winningMessage: 'You Lose! Scissors beats Paper',
                     winner: 'computer',
                 },
             ];
-        } else if (playerChoice === '✌' && computerChoice === '✋') {
+        } else if (playerSelection === '✌' && computerSelection === '✋') {
             return [
                 {
                     winningMessage: 'You Won! Scissors beats Paper',
@@ -93,19 +122,16 @@ const playRound = function (playerChoice, computerChoice) {
 let gameCount = 0;
 let playerWinningCount = 0;
 let computerWinningCount = 0;
-const optionsPlayerHave = document.querySelectorAll('.options__box');
+const optionsPlayerHave = document.querySelectorAll('.option__box');
 optionsPlayerHave.forEach((option) => {
     option.addEventListener('click', () => {
         const computerChoiceEl = document.getElementById('rps-output-computer');
         const computerSelection = getComputerChoice();
         const currentGameUpdate = document.querySelectorAll('.updateMessage');
-        const currentPlayerScore = document.getElementById('score-player');
-        const currentComputerScore = document.getElementById('score-computer');
+        const playerScore = document.getElementById('score-player');
+        const computerScore = document.getElementById('score-computer');
 
         computerChoiceEl.textContent = computerSelection;
-
-        //Incrementing gameCount by 1
-        gameCount++;
 
         if (gameCount <= 5) {
             let [currentResults] = playRound(
@@ -115,53 +141,30 @@ optionsPlayerHave.forEach((option) => {
             let currentWinner = currentResults.winner;
             let currentWinningMessage = currentResults.winningMessage;
 
-            if (currentWinningMessage !== 'Game Tied!') {
+            // Only go in this statement if the winning message is NOT tied
+            if (currentWinningMessage !== 'Game tied!') {
                 currentGameUpdate.textContent = currentWinningMessage;
-                currentGameUpdate.style.backgroundColor = '#f8e700;';
-                currentGameUpdate.style.Color = '#fff;';
+                currentGameUpdate.style.color = '#f8e700';
+                console.log('Current winner:', currentWinner); // Add this line to check the currentWinner
+
                 if (currentWinner === 'player') {
                     playerWinningCount++;
-                    currentPlayerScore.textContent = playerWinningCount;
+                    playerScore.textContent = playerWinningCount;
                 } else if (currentWinner === 'computer') {
                     computerWinningCount++;
-                    currentComputerScore.textContent = computerWinningCount;
+                    computerScore.textContent = computerWinningCount;
                 }
-            } else {
-                currentGameUpdate.textContent = currentWinningMessage;
+
+                // Incrementing the game count with 1
+                gameCount++;
             }
-        } else {
-            if (playerWinningCount > computerWinningCount) {
-                currentGameUpdate.textContent = 'GAME OVER! Congrats you won.';
-                currentGameUpdate.style.playing = false;
+            // If game is tied go in this statement
+            else {
+                currentGameUpdate.textContent = currentWinningMessage;
+                // currentGameUpdate.style.color = '#fa4f00';
             }
         }
     });
-});
-
-const rockOption = document.getElementById('✊');
-const paperOption = document.getElementById('🤚');
-const scissorOption = document.getElementById('✌');
-const playerChoiceEl = document.getElementById('rps-output-player');
-
-rockOption.addEventListener('click', function () {
-    playRound('✊', computerSelection);
-    playerSelection = '✊';
-    console.log(playerSelection);
-    playerChoiceEl.textContent = playerSelection;
-});
-
-paperOption.addEventListener('click', function () {
-    playRound('🤚', computerSelection);
-    playerSelection = '🤚';
-    console.log(playerSelection);
-    playerChoiceEl.textContent = playerSelection;
-});
-
-scissorOption.addEventListener('click', function () {
-    playRound('✌', computerSelection);
-    playerSelection = '✌';
-    console.log(playerSelection);
-    playerChoiceEl.textContent = playerSelection;
 });
 
 // const playerNameEl = document.getElementById("player-name");
